ca4f86386a46c7725b2fbb0d2e8a1bc2067eb6bf
unknown <sanchitsondhi2005@gmail.com>
2023-07-14 13:28:35 +0530

    checking today's work

--- tut3.cpp
+++ tut3.cpp
@@ -6,9 +6,17 @@ using namespace std;
 // multi
 // line 
 // task
+int sum(int a, int b){
+    return a+b;
+}
+
 int main(){
-    float sum = 6.1;
-    cout<<"sanchit"<<sum;
+    // float sum = 6.1;
+    // cout<<"sanchit"<<sum;
+    int a,b;
+    cin>>a>>b;
+
+    cout<<"Sum of 2 numbers is: "<< sum(a, b);
     return 0;
     // this is created by the multi site vlogger 
     // and the code is given by code with harry 
