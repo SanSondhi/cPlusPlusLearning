6c819ee10ceb6cae54fe5fe321e0365a6bf017d6
unknown <sanchitsondhi2005@gmail.com>
2023-07-17 17:56:22 +0530

    17 july vmm

---
+++ tut20.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class binary
+{
+    string s;
+
+public:
+    void read(void);
+    void chk_bin(void);
+};
+
+int main()
+{
+    // OOPs - classes and objects
+
+    // c++ --> initially called --> stroustroup
+    // class -->extension of structures (in c)
+    //  structures and limitations
+    //       - members are public
+    //       - no methods
+    //  classes --> structures + more
+    //  classes --> can have methods and properties
+    //  classes --> can make few members private and few public
+    //  structures in c++ in typedefed
+    //  you can declare objects with the class declaration like this
+    /*class Employee{
+        //class defimition
+    } harry, roman, lavish*/
+    // harry.salary  8 makes no sense if salary is private
+
+    // nesting of member functions
+    class binary
+    {
+        string s;
+
+    public:
+        void read(void);
+        void chk_bin(void);class  
+        int 
+    }
+
+    return 0;
+}
--- tut7.cpp
+++ tut7.cpp
@@ -1,12 +1,56 @@
 #include<iostream>
-using namespace std;
+ using namespace std;
+
+ int c = 45;
 
 int main(){
-    int a=6; 
-    int b=7;
-    float pi= 3.14;
+    // **********Built in data types*************
 
-    cout<<"the total number of students in the class are "<<a+b;
-    cout<<"the value of pi is"<<pi;
+    // int a,b,c;
+    // cout<<"enter the value of a: "<<endl;
+    // cin>>a;  
+    // cout<<"enter the value of b: "<<endl;
+    // cin>>b;
+
+    // c=a+b;
+    // cout<<"the sum is " <<c<<endl;
+    // cout<<"the global c is " <<::c;
+
+    // // **********float,double and long double literals**********
+    
+    // float d = 34.4f;             
+    // long double e =34.4L;
+    // cout<<"the size of 34 is "<<sizeof(34.4f)<<endl;
+    // cout<<"the size of 34f is "<<sizeof(34.4f)<<endl;
+    // cout<<"the size of 34F is "<<sizeof(34.4f)<<endl;
+    // cout<<"the size of 34l is "<<sizeof(34.4f)<<endl;
+    // cout<<"the size of 34L is "<<sizeof(34.4f)<<endl;
+    // cout<<"the value of d is "<<d<<endl<<"the value of e is "<<e;
+
+    // // **************Reference variables*******************
+    // // rohan das----> monty -----> rohu ----> dangerous coder
+    // float x = 455;
+    // float & y = x;
+    // cout<<x<<endl;
+    // cout<<y<<endl;
+
+    //************** Typercasting *****************************
+
+    int a= 45;
+    float b = 45.46;
+    cout<<"the value of a is "<<(float)a<<endl;
+    cout<<"the value of a is "<<float(a)<<endl;
+
+
+    cout<<"the value of b is "<<(int)b<<endl;
+    cout<<"the value of b is "<<int(b)<<endl;
+    int c = int(b);
+
+    cout<<"the expression is "<<a+ b<<endl;
+    cout<<"the expression is "<<a+ int(b)<<endl;
+    cout<<"the expression is "<<a+ (int)b<<endl;
+
+
+
     return 0;
-}
+ }
