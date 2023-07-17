ac788ce859f1a98d15fba1e163777e93b6ef052c
unknown <sanchitsondhi2005@gmail.com>
2023-07-17 13:29:32 +0530

    17 july

--- tut14.cpp
+++ tut14.cpp
@@ -11,10 +11,16 @@ int main(){
     // cout<<"the value of a was "<<a;
     //  a = 45; // you will get error because a is a constant
     // cout<<"the value of a is "<<a;
+
+    // manuplaters in c++
     int a = 3 ,b= 78 , c=1233;
-    cout<<"the value of a is "<<a<<endl;
-    cout<<"the value of b is "<<b<<endl;
-    cout<<"the value of c is "<<c<<endl;
+    cout<<"the value of a without setw is "<<a<<endl;
+    cout<<"the value of b without setw  is "<<b<<endl;
+    cout<<"the value of c without setw  is "<<c<<endl;
+
+    cout<<"the value of a is "<<setw(4)<<a<<endl;
+    cout<<"the value of b is "<<setw(4)<<b<<endl;
+    cout<<"the value of c is "<<setw(4)<<c<<endl;
     return 0;
 
 }
