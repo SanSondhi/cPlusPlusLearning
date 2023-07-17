a4a4001981c676fcd30bcd699e3d5d5d5733cbcd
unknown <sanchitsondhi2005@gmail.com>
2023-07-17 13:21:16 +0530

    17 july

--- tut14.cpp
+++ tut14.cpp
@@ -6,11 +6,15 @@ int main(){
     // a = 45;
     // cout<<"the value of a is "<<a;
 
-    // constants in c++
-    float a = 3.11;
-    cout<<"the value of a was "<<a;
-    a = 45;
-    cout<<"the value of a is "<<a;
+    //  constants in c++
+    // const int = 3.11;
+    // cout<<"the value of a was "<<a;
+    //  a = 45; // you will get error because a is a constant
+    // cout<<"the value of a is "<<a;
+    int a = 3 ,b= 78 , c=1233;
+    cout<<"the value of a is "<<a<<endl;
+    cout<<"the value of b is "<<b<<endl;
+    cout<<"the value of c is "<<c<<endl;
     return 0;
 
 }
