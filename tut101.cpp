601038a36d7f3df8f8c888f7947e669146a424db
unknown <sanchitsondhi2005@gmail.com>
2023-07-30 12:49:22 +0530

    30th july

---
+++ tut101.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number "<<endl;
+    cin>>n;
+    if (n>=0)
+    cout<<"Positive number ";
+    else 
+    cout<<"negative number";
+    return 0;
+}
